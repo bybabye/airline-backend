4be95d1a25f775fb49fdb4e87dae188863cb53ab
bybabye <89548252+bybabye@users.noreply.github.com>
2023-05-27 18:33:20 +0700

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const addUser = (req, res) => { // thêm mới chuyến bay dành cho admin
     const data = req.body; // lấy dữ liệU từ client gửi về
-    const query = `INSERT INTO khachhang ( tenkh, ngaysinh, cmnd , noicap,quoctich,ngonngu)
+    const query = `INSERT INTO khachhang ( tenkh, ngaysinh, cmnd , noicap,quoctich,ngonngu,gmail)
     SELECT '${data.tenkh}', '${data.ngaysinh}','${data.cmnd}','${data.noicap}' , ${data.quoctich},${data.ngonngu},${data.gmail}
     WHERE NOT EXISTS (
         SELECT 1 FROM khachhang 
